408ad93c46fee8268592a2f893d85071d5e6a1c5
sanich123 <aovoronin.piano@gmail.com>
2022-09-10 17:24:11 +0300

    Some refactoring with whitespace

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,28 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles (names) {
-    const result = [];
-    const map = {};
-    for (let i = 0; i < names.length; i++) {
-        const curr = names[i];
-        if (!map[curr]) {
-            if (!result.includes(curr)) {
-                result.push(curr);
-                map[curr] = 0;
-            } else {
-                map[curr] = 1;
-                result.push(`${curr}(${map[curr]})`)
-                }
-        } 
-        else {
-            map[curr]++;
-            result.push(`${curr}(${map[curr]})`)
-        }
+function renameFiles(names) {
+  const result = [];
+  const map = {};
+  for (let i = 0; i < names.length; i++) {
+    const curr = names[i];
+    if (!map[curr]) {
+      if (!result.includes(curr)) {
+        result.push(curr);
+        map[curr] = 0;
+      } else {
+        map[curr] = 1;
+        result.push(`${curr}(${map[curr]})`);
+      }
+    } else {
+      map[curr]++;
+      result.push(`${curr}(${map[curr]})`);
     }
-    return result;
+  }
+  return result;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
