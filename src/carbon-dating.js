b3708fa7e801b2131d27b537b3bbbddf4dafd4ce
Alexander Voronin <aovoronin.piano@gmail.com>
2022-09-11 19:43:26 +0300

    Solved carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,24 +6,31 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
 function dateSample(string) {
+    const numFromStr = Number.parseFloat(string, 10);
 
-    return Math.ceil(Math.log(MODERN_ACTIVITY / +string) / (0.693 / HALF_LIFE_PERIOD))
+  if (
+    typeof string !== 'string' || 
+    !string || 
+    Number.isNaN(numFromStr) ||
+    numFromStr === NaN || 
+    numFromStr > 15 ||
+    numFromStr <= 0) return false;
+
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +string) / (0.693 / HALF_LIFE_PERIOD));
 }
-console.log(dateSample('11'))
- 
 
 module.exports = {
-  dateSample
+  dateSample,
 };
