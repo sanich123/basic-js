8d52e1ff3fa45c20c91a121cf06c87e558285633
sanich123 <aovoronin.piano@gmail.com>
2022-09-13 14:49:20 +0300

    Almost done with vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,29 +19,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+class VigenereCipheringMachine {
+  constructor(condition = true) {
+    this.condition = condition;
+  }
+  _getNonAlphabet(phrase) {
+    const map = new Map();
+    for (let i = 0; i < phrase.length; i++) {
+      if (/[^a-z]/gi.test(phrase[i])) {
+        map.set(`${phrase[i]} + ${i}`, i);
+      }
+    }
+    return map;
+  }
+  _getCodeToAllLength(cleanedWord, code) {
+    const length = Math.floor(cleanedWord.length / code.length);
+    const rest = cleanedWord.length % code.length;
+    return `${code.repeat(length)}${code.slice(0, rest)}`;
+  }
 
-function vigenereCipher(string1, string2) {
-    let result = '';
-
-    for (let i = 0; i < string1.length; i++) {
-        const firstIndex = string1[i].charCodeAt() - 97;
-        const secondIndex = string2[i].charCodeAt() - 97;
-        const cipher = (firstIndex + secondIndex) % 26;
-        result += String.fromCharCode(cipher + 97)
+  _insertNonAlphabetSymbols(result, nonAlphabetSymbols) {
+    for (const [key, value] of nonAlphabetSymbols) {
+      result.splice(value, 0, key[0]);
     }
     return result;
-}
-
-console.log(vigenereCipher('attack a', 'alphonse'))
-console.log(`${String.fromCharCode(97)}`)
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(phrase, code) {
+    if (!phrase || !code) {
+      throw new Error("Incorrect arguments!");
+    }
+    const result = [];
+    const cleanedWord = phrase.replace(/[^a-z]/gi, "");
+    const codeToAllLength = this._getCodeToAllLength(cleanedWord, code);
+    const nonAlphabetSymbols = this._getNonAlphabet(phrase);
+
+    for (let i = 0; i < cleanedWord.length; i++) {
+      const numberOfChar = cleanedWord[i].charCodeAt() - 65;
+      const numberOfCharInKey = codeToAllLength[i].charCodeAt() - 97;
+      const decryptCipher = (numberOfChar - numberOfCharInKey + 26) % 26;
+      result.push(String.fromCharCode(decryptCipher + 65));
+    }
+    if (this.condition) {
+      return this._insertNonAlphabetSymbols(result, nonAlphabetSymbols).join("");
+    } else {
+      return this._insertNonAlphabetSymbols(result, nonAlphabetSymbols).reverse().join("");
+    }
+  }
+
+  encrypt(phrase, code) {
+    if (!phrase || !code) {
+      throw new Error("Incorrect arguments!");
+    }
+    const result = [];
+    const cleanedWord = phrase.replace(/[^a-z]/gi, "");
+    const codeToAllLength = this._getCodeToAllLength(cleanedWord, code);
+    const nonAlphabetSymbols = this._getNonAlphabet(phrase);
+
+    for (let i = 0; i < cleanedWord.length; i++) {
+      const numberOfChar = cleanedWord[i].charCodeAt() - 97;
+      const numberOfCharInKey = codeToAllLength[i].charCodeAt() - 97;
+      const encryptCipher = (numberOfChar + numberOfCharInKey) % 26;
+      result.push(String.fromCharCode(encryptCipher + 65));
+    }
+    if (this.condition) {
+      return this._insertNonAlphabetSymbols(result, nonAlphabetSymbols).join("");
+    } else {
+      return this._insertNonAlphabetSymbols(result, nonAlphabetSymbols).reverse().join("");
+    }
   }
 }
 
